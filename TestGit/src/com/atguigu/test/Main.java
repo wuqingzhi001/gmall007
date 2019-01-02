acc74e17bc26e4d21901e1a065cb875a9eb3d514
wuqingzhi <HeartisTiger@163.com>
2019-01-02 18:49:14 +0800

    dev02的第一次提交

--- TestGit/src/com/atguigu/test/Main.java
+++ TestGit/src/com/atguigu/test/Main.java
@@ -8,5 +8,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("git.....");
         System.out.println("go.....");
+        System.out.println("我是最帅的");
     }
 }
