91332a5d454b6e5587ee59500e947720b0a6e88f
wuqingzhi <HeartisTiger@163.com>
2019-01-02 18:24:30 +0800

    idea测试第一次提交

---
+++ TestGit/src/com/atguigu/test/Main.java
@@ -0,0 +1,11 @@
+package com.atguigu.test;
+
+/**
+ * @aythor HeartisTiger
+ * 2019-01-02 18:21
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("git.....");
+    }
+}
